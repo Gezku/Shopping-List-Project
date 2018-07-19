8ddf530e1e0d55ec2f17e353bf5b6f030a676480
Gezku <onlyautumnshade@gmail.com>
2018-07-19 00:09:56 -0500

    Added new functionality
    it will tell you if u forget to introduce an item name, price or quantity of items

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 /* Future functionality 
 
-
+-Check if the inputs are empty
 -Add de quantity of items
 -Change the list for a table
 -Name a list
@@ -11,79 +11,75 @@
 -Store list 
 -Put the hour when it was created
 
-
-
-
 */
-
-
-
-
-
-
 
 
 var selectButton = document.querySelector("button");
-var selelectCreateListBtn = document.getElementById("createListName");
-
-/*
-var selectli = document.getElementById("lista-uno");
-var selectli2= document.getElementById("lista-2");
-Experiment
-*/
 
 
-
+//var selelectCreateListBtn = document.getElementById("createListName");   This var is a test for the moment 18 june 2018
 
 
 
 selectButton.addEventListener("click", function(){
-    
-    var itemName = document.getElementById("itemName").value;       // Gets the value of the input element with the "itemName" ID
-    
-    var createListItem = document.createElement("li");              // Creates an item list.
-    var convertText= document.createTextNode(itemName + "    ");    // Creates a string or text that we can insert in our item list.
-    
-    var createButton = document.createElement("button");            // Creates a button element.
-    var createTextBtn = document.createTextNode("Remove");          // Creates a string or text that we can insert in our button.
-    
-    var select_UL = document.querySelector("ul");                   // We select the Ul (unordered list) that is created in the index html, so we can insert the list item.
-    
-    var checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    
-    var priceInput = document.getElementById("itemPrice").value;
-    var priceInputText = document.createTextNode(priceInput + " ");
-    
-    var quantityInput = document.getElementById("itemQuantity").value;
-    var quantityInputText = document.createTextNode(quantityInput + " ");
 
+    //Values
     
+    var itemName = document.getElementById("itemName").value;               // Gets the value of the input element with the "itemName" ID
+    var priceInput = document.getElementById("itemPrice").value;            // Gets the value of the input element with the "itemPrice" ID
+    var quantityInput = document.getElementById("itemQuantity").value;      // Gets the quantityInput element with the "itemQuantity" ID
+
+    //Creations
+    
+    var createListItem = document.createElement("li");                      // Creates an item list.
+    var convertText= document.createTextNode(itemName + "    ");            // Creates a string or text that we can insert in our item list.
+    var createButton = document.createElement("button");                    // Creates a button element.
+    var createTextBtn = document.createTextNode("Remove");                  // Creates a string or text that we can insert in our button.
+    var checkbox = document.createElement("input");                         // Creates a checkbox
+    var priceInputText = document.createTextNode(priceInput + " ");         // Gets the value from priceInput and converts it into a string or text.
+    var quantityInputText = document.createTextNode(quantityInput + " ");   // Gets the value from quantityInput and converts it into a string or text.
+
+    //Selections
+
+    var select_UL = document.querySelector("ul");                           // We select the Ul (unordered list) that is created in the index html, so we can insert the list item.
+    
+    //Change attributes of elements
+    
+    checkbox.setAttribute("type", "checkbox");                              // Changes the type of the checkbox 
    
-
-
- 
-
-    createButton.appendChild(createTextBtn);                        // Append or insert the textnode in our Button
-    createListItem.appendChild(convertText);                        // Append or insert the text node in our list
-    createListItem.appendChild(priceInputText);
-    createListItem.appendChild(quantityInputText);
-    createListItem.appendChild(createButton);                       // Append or insert the button in the list item
     
-    createListItem.appendChild(checkbox);
-    select_UL.appendChild(createListItem);                          // Insert or append the the list item inside the Ul that was created in the index file.
+    // if you didnt put an item name value, price value or quantity of products, a message will pop out, and items will not be created. 
 
-    itemName = document.getElementById("itemName").value= "";       // Remove the value of the input, so we can insert another item list.
-    priceInput = document.getElementById("itemPrice").value= "";  
-    quantityInput = document.getElementById("itemQuantity").value= "";  
+    if (itemName === ""){
+        alert("Insert an item, please...");
+    }else if(priceInput == ""){
+        alert("Insert a price, please...")
+    }else if (quantityInput == ""){
+        alert("Insert quantity of items, please...")
+    }else{
+        // Append elements
+        
+        createButton.appendChild(createTextBtn);                              // Append or insert the textnode in our Button
+        createListItem.appendChild(convertText);                              // Append or insert the text node in our list
+        createListItem.appendChild(priceInputText);                           // Append or insert the text node of priceInputText into the li
+        createListItem.appendChild(quantityInputText);                        // Append or insert the text node of quantityInputText into the li
+        createListItem.appendChild(createButton);                             // Append or insert the button in the li
+        createListItem.appendChild(checkbox);                                 // Append or insert the checkbox in the li
+        select_UL.appendChild(createListItem);                                // Insert or append the the list item inside the Ul that was created in the index file.
+
+        //Reset input values to ""
+        itemName = document.getElementById("itemName").value= "";       
+        priceInput = document.getElementById("itemPrice").value= "";  
+        quantityInput = document.getElementById("itemQuantity").value= "";
+        createListItem.style.cssText = "border: black solid 1px; width: 300px; margin-bottom: 2px;"  
+
+    }
 
     checkbox.addEventListener("click", function(){
         if (checkbox.checked == true){
             createListItem.style.textDecoration = "line-through";
-            console.log("Verdadero");
         }else{
             createListItem.style.textDecoration = "none";
-            console.log("Falso");
         }
     });
 
@@ -93,12 +89,19 @@ selectButton.addEventListener("click", function(){
     });
     
 
+  
+
+
+
+
 
 
 });
 
 
 
+//This function is a test for the moment. 18 june 2018
+/*
 selelectCreateListBtn.addEventListener("click",function(){
     var newH2 = document.createElement("h2");
     var newLi = document.createElement("li");
@@ -117,12 +120,16 @@ selelectCreateListBtn.addEventListener("click",function(){
     
     selectMenuList.appendChild(newLi);
     newLi.style.cssFloat = "left";
+
+    var selectMierda = selectMenuList.item(0);
+
+    console.log(selectMierda);
     
 
 });
 
 
-
+*/
 
 
 
